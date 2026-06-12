69808adb75e2e40b7ef5e0c59ef8e8c89f10ec42
Amir Hesamian <hesamian@uwm.edu>
2026-06-12 15:05:16 -0500

    fix: add missing EfRepository entity profiles for all entities

---
+++ Api/Data/Profiles/EntityProfiles.cs
@@ -0,0 +1,24 @@
+using Api.Data.Entities;
+using EfCoreRepository;
+
+namespace Api.Data.Profiles;
+
+public class SmsConnectionProfile : EntityProfile<SmsConnection>
+{
+    public SmsConnectionProfile() { MapAll(); }
+}
+
+public class SmsMessageProfile : EntityProfile<SmsMessage>
+{
+    public SmsMessageProfile() { MapAll(); }
+}
+
+public class WebhookSubscriptionProfile : EntityProfile<WebhookSubscription>
+{
+    public WebhookSubscriptionProfile() { MapAll(); }
+}
+
+public class ApiTokenProfile : EntityProfile<ApiToken>
+{
+    public ApiTokenProfile() { MapAll(); }
+}
